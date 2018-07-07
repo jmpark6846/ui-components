321896d71025771c42a67bf3553d7ad13c3906ab
jmpark6846 <jmpark6846@gmail.com>
2018-07-07 21:51:08 +0900

    index 추가

---
+++ index.js
@@ -0,0 +1,21 @@
+import Divider from './Divider/Divider'
+import { Col, Row, Container } from "./grid";
+
+import Header from './Header/Header'
+import Section from './Section/Section'
+import SectionHeader from "./SectionHeader/SectionHeader"; 
+import SubHeader from './SubHeader/SubHeader'
+
+export {
+  Divider,
+  Col,
+  Row,
+  Container,
+
+  Header,
+  Section,
+  SectionHeader,
+  SubHeader,
+}
+
+
