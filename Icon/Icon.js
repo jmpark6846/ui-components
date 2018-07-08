f85ad276c321c26c954a197da381116fcce64596
jmpark6846 <jmpark6846@gmail.com>
2018-07-08 20:51:29 +0900

    toggleButton을 navmenu 내부에 추가

--- Icon/Icon.js
+++ Icon/Icon.js
@@ -5,7 +5,7 @@ import './Icon.css'
 const Icon = ({className, onClick, icon}) => {
   const classes = classNames('icon', className, icon)
   return (
-    <span className={classes} onClick={onClick}>룰루</span>
+    <span className={classes} onClick={onClick}>토글  </span>
   )
 }
 
--- NavBar/NavMenu.js
+++ NavBar/NavMenu.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import ToggleButton from './ToggleButton';
 import './NavMenu.css'
 
-const NavMenu = ({toggle, pullRight, children}) => {
-  const classes = classNames('nav-menu', {'ml-auto': pullRight }, {'hide':toggle})
-  return (
-    <ul className={classes}>
-      {children}
-    </ul>
-  )
+class NavMenu extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state={ open: false }
+  }
+
+  handleToggle = () => {
+    this.setState({open:!this.state.open})
+  
+  }
+  render(){
+    const {pullRight, children} = this.props
+    const classes = classNames('nav-menu', {'ml-auto': pullRight }, {'hide': !this.state.open})
+    return (
+      <React.Fragment>
+        <ToggleButton onClick={this.handleToggle}/>
+        <ul className={classes}>
+          {children}
+        </ul>
+      </React.Fragment>
+    )
+  }
+  
 }
 
 NavMenu.propTypes = {
