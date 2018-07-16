62a7ed4a9e8ea58b5cd0c74e3ee5e3e808ff574d
jmpark6846 <jmpark6846@gmail.com>
2018-07-16 15:21:40 +0900

    Rating을 클래스 컴포넌트로 수정

--- Rating/Rating.js
+++ Rating/Rating.js
@@ -5,36 +5,38 @@ import Icon from '../Icon/Icon'
 import './Rating.css'
 
 const maxRating = 5
-const Rating = ({rating, className, reviewCount}) => {
-  const star = parseInt(rating)
-  const half = rating*10%10 !== 0 ? 1 : 0
-  const arr = Array(maxRating).fill('star', 0, star).fill('half', star, star+half).fill('blank', star+half, maxRating)
-  // 2.3
-  // 별별반빈빈
-  // 5
-  // 4 별별별별빈
-  return (
-    <div className={classNames('rating', className)}>
-      {arr.map((s,i)=>{
-        if(s === 'star')
-          return <Icon key={i} icon='fas fa-star' />
-        else if(s === 'half')
-          return <Icon key={i} icon='fas fa-star-half-alt' />
-        else 
-          return <Icon key={i} icon='far fa-star' />
-      })}
-      { reviewCount &&
-        <span className='review-count'>
-          구매평 {reviewCount}
-        </span>
-      }
-    </div>
-  )
+class Rating extends React.Component{
+
+  render(){
+    const { value, className, reviewCount } = this.props
+    const star = parseInt(value)
+    const half = value*10%10 !== 0 ? 1 : 0
+    const arr = Array(maxRating).fill('star', 0, star).fill('half', star, star+half).fill('blank', star+half, maxRating)
+
+    return (
+      <div className={classNames('rating', className)}>
+        {arr.map((s,i)=>{
+          if(s === 'star')
+            return <Icon key={i} icon='fas fa-star' />
+          else if(s === 'half')
+            return <Icon key={i} icon='fas fa-star-half-alt' />
+          else 
+            return <Icon key={i} icon='far fa-star' />
+        })}
+        { reviewCount &&
+          <span className='review-count'>
+            구매평 {reviewCount}
+          </span>
+        }
+      </div>
+    )
+  } 
 }
 
 Rating.propTypes = {
-  rating: PropTypes.number,
+  value: PropTypes.number,
   reviewCount: PropTypes.number,
+  className: PropTypes.string,
 }
 
 export default Rating
